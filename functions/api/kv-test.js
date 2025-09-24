08c01946fb40f9abea29ad7c30a78ddb6fb97b69
netm <takrahaifu@gmail.com>
2025-09-24 22:20:14 +0900

    Deploy to Cloudflare Pages

--- functions/api/kv-test.js
+++
@@ -1,8 +0,0 @@
-export async function onRequestGet(context) {
-  await context.env.FRIDGE_KV.put("kv_test_key", "hello-fridge");
-  const value = await context.env.FRIDGE_KV.get("kv_test_key");
-  return new Response(JSON.stringify({ ok: true, value }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
-}
